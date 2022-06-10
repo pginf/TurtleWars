c4cffb3de828921282e7a945a60022c9be984d3f
Kacper <kaper02@o2.pl>
2022-06-10 22:13:20 +0200

    Added game object group and edited game object

---
+++ core/game_obeject_group.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from enum import Enum
+
+
+class GameObjectGroup(Enum):
+    NONE = 0
+    ENVIRONMENT = 1
+    PLAYER = 2
--- core/game_object.py
+++ core/game_object.py
@@ -1,8 +1,30 @@
 from __future__ import annotations
+from core import GameObjectGroup
+
 
 class GameObject:
-
-
+    _position: float
+    _scale: float
+    _rotation: float
+    _group: GameObjectGroup = GameObjectGroup.NONE
 
     def __init__(self):
         pass
+
+    def set_position(self, position):
+        self._position = position
+
+    def get_position(self):
+        return self._position
+
+    def set_scale(self, scale):
+        self._scale = scale
+
+    def get_scale(self):
+        return self._scale
+
+    def set_rotation(self, rotation):
+        self._rotation = rotation
+
+    def get_rotation(self):
+        return self._rotation
